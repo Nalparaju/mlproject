17cf41492fc9420215727da52ad1c2cfabac31f6
all_is_will <114322465+Nalparaju@users.noreply.github.com>
2025-06-11 19:52:50 -0700

    Data Transformation

--- src/exception.py
+++ src/exception.py
@@ -11,7 +11,7 @@ def error_message_detail(error, error_details:sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_details:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)  
         self.error_message = error_message_detail(error_message,error_details=error_details)
 
     def __str__(self):
