b4afd8875dd1271e9a82cdfbc52c9549c73b7c7e
all_is_will <114322465+Nalparaju@users.noreply.github.com>
2025-06-18 23:45:34 -0700

    HyperParameter Training

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     """
@@ -22,7 +23,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     """
     This function evaluates the given models and returns a report of their performance.
     """
@@ -30,8 +31,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
